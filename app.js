2543e01fa1eec087738f9c3bb0fcfda1a67d8906
Alice Dias <pereira.dias.alice@escola.pr.gov.br>
2025-10-10 11:42:25 -0300

    ok

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let numeroSecreto = gerarNumeroAleatorio();
 
-
 function exibirTextoNaTela(tag, texto) {
    let campo = document.querySelector(tag);
    campo.innerHTML = texto;
+}
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
