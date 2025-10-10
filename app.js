3d4c09a777eb4f17bb92b9e4e03884d1a8ca22a5
Alice Dias <pereira.dias.alice@escola.pr.gov.br>
2025-10-10 11:48:11 -0300

    ok

--- app.js
+++ app.js
@@ -8,18 +8,17 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute() {
-   let chute = document.querySelector('input').value;
-  
-   if (chute == numeroSecreto) {
-               exibirTextoNaTela('h1', 'Acertou!');
-               exibirTextoNaTela('p', 'Você descobriu o número secreto!');
-        } else {
-                if (chute > numeroSecreto) {
-                       exibirTextoNaTela('p', 'O número secreto é menor');
-                } else {
-                       exibirTextoNaTela('p', 'O número secreto é maior');
-               }
-       }
-}
+//Código omitido
 
+if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let mensagemTentativas = `Você descobriu o número secreto com  ${tentativas} tentativas!`;
+        exibirTextoNaTela('p', 'Você descobriu o número secreto com 5 tentativas!');
+        } else {
+                        if (chute > numeroSecreto) {
+                                        exibirTextoNaTela('p', 'O número secreto é menor');
+                        } else {
+                                        exibirTextoNaTela('p', 'O número secreto é maior');
+                        }
+        }
+}
