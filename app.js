5fa69eb1505156e9167544a9effaba73d3480b6c
Alice Dias <pereira.dias.alice@escola.pr.gov.br>
2025-10-10 11:50:43 -0300

    ok

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 let numeroSecreto = gerarNumeroAleatorio();
 
+
 function exibirTextoNaTela(tag, texto) {
    let campo = document.querySelector(tag);
    campo.innerHTML = texto;
 }
 
+
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
-//Código omitido
 
-if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!');
-        let mensagemTentativas = `Você descobriu o número secreto com  ${tentativas} tentativas!`;
-        exibirTextoNaTela('p', 'Você descobriu o número secreto com 5 tentativas!');
-        } else {
-                        if (chute > numeroSecreto) {
-                                        exibirTextoNaTela('p', 'O número secreto é menor');
-                        } else {
-                                        exibirTextoNaTela('p', 'O número secreto é maior');
-                        }
-        }
+function verificarChute() {
+   let chute = document.querySelector('input').value;
+  
+   if (chute == numeroSecreto) {
+       exibirTextoNaTela('h1', 'Acertou!');
+               exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+       } else {
+               if (chute > numeroSecreto) {
+                       exibirTextoNaTela('p', 'O número secreto é menor');
+               } else {
+                       exibirTextoNaTela('p', 'O número secreto é maior');
+               }
+       }
 }
+
